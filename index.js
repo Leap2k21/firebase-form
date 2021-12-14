ec7940416c7e419839f6d8716f7134418308e29a
Sekeee <setgelll1212@gmail.com>
2021-12-14 23:33:07 +0800

    done

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 const $button = document.querySelector('button');
+const collectionName = 'Nest';
+const db = firebase.firestore();
 
-function onClick(){
-    console.log('hha')
+
+
+function onClick() {
     $name = document.querySelector('.name');
     let Name = $name.value;
-    
     $age = document.querySelector('.age');
     let Age = $age.value;
+    $id = document.querySelector('.id');
+    let id = $id.value;
 
-        const db = firebase.firestore();
-    db.collection('Nest').doc('1').set({
-        name: Name , 
-        age : Age
-    }).then(() => {
+    db.collection(collectionName).doc(`${id}`).set({
+        name: Name,
+        age: Age,
+    }).then(doc => {
         console.log("successfully written!");
-    })  
-
+    })
     $name.value = "";
     $age.value = "";
+    $id.value = "";
+    renderDocs();
 }
+
+function renderDocs(){
+    db.collection(collectionName).get().then(data => {
+        data.forEach(doc => {
+            console.log(doc.data());
+            $span1 = document.createElement('span');
+            $span2 = document.createElement('span');
+            $div = document.createElement("div");
+            $body = document.querySelector("body");
+            $div.append($span1 , $span2);
+            $body.append($div);
+
+            $span1.innerText = doc.data().name;
+            $span2.innerText = doc.data().age;
+        }); 
+    })
+
+
+}
+
+
